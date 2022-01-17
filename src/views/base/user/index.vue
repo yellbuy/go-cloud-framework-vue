03ab4941aa7f02b881840e897f19af8af3c97fff
YELLBUY <19892257@qq.com>
2022-01-17 18:02:37 +0800

    base 用户排序字段列表显示

--- src/views/base/user/index.vue
+++ src/views/base/user/index.vue
@@ -52,7 +52,11 @@
 						<el-tag type="danger" size="mini" v-else>{{ $t('message.action.disable') }}</el-tag>
 					</template>
 				</el-table-column>
+				<el-table-column prop="Order" label="排序" width="80" align="right" show-overflow-tooltip>
+				</el-table-column>
 				<el-table-column prop="CreateTime" label="创建时间" :formatter="dateFormatYMDHM" show-overflow-tooltip>
+				</el-table-column>
+				<el-table-column prop="LoginTime" label="最后登录时间" :formatter="dateFormatYMDHM" show-overflow-tooltip>
 				</el-table-column>
 				<el-table-column label="操作" width="180" fixed="right">
 					<template #default="scope">
