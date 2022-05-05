b6983039f93099a48816d5538d5e35846fb1032b
YELLBUY <19892257@qq.com>
2022-05-05 18:22:34 +0800

    流程设计器集成

---
+++ src/i18n/pages/meta/en.ts
@@ -0,0 +1,12 @@
+// 定义内容
+export default {
+	router: {
+		workflow: 'workflow',
+		form: 'form',
+		reporter: 'reporter',
+		bigScreen:'big screen',
+	},
+	action:{
+	}
+	
+};
---
+++ src/i18n/pages/meta/zh-cn.ts
@@ -0,0 +1,12 @@
+// 定义内容
+export default {
+	router: {
+		workflow: '自定义流程',
+		form: '自定义表单',
+		reporter: '自定义报表',
+		bigScreen:'自定义大屏',
+	},
+	action:{
+	}
+	
+};
