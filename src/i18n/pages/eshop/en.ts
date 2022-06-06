512041779f158e89a0d432f451c58ba444d8bc56
lavender <839997461@qq.com>
2022-06-06 18:02:31 +0800

    eshop_订单管理

--- src/i18n/pages/eshop/en.ts
+++ src/i18n/pages/eshop/en.ts
@@ -3,7 +3,7 @@ export default {
 	router: {
 		virtualGoods: 'virtual goods',
 	},
-	action:{
+	action: {
 	}
-	
+
 };
--- src/i18n/pages/eshop/zh-cn.ts
+++ src/i18n/pages/eshop/zh-cn.ts
@@ -2,8 +2,9 @@
 export default {
 	router: {
 		virtualGoods: '虚拟商品',
+		orderManagement: '订单管理'
 	},
-	action:{
+	action: {
 	}
-	
+
 };
