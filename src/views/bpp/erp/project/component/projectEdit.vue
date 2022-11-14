634dd24e20a048cc20de1e151a45320150174080
lavender <839997461@qq.com>
2022-11-14 17:45:05 +0800

    招采系统完善

--- src/views/bpp/erp/project/component/projectEdit.vue
+++ src/views/bpp/erp/project/component/projectEdit.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="system-edit-user-container">
-		<el-dialog :title="title" v-model="isShowDialog" width="80%" :before-close="onCancel">
+		<el-dialog :title="title" v-model="isShowDialog" :destroy-on-close="true" width="80%" :before-close="onCancel">
 			<el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" size="small" label-width="120px" v-loading="loading">
 				<el-row :gutter="20">
 					<el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8" class="mb10">
@@ -30,7 +30,7 @@
 							<el-checkbox v-model="ruleForm.RemoteState" :true-label="1" :false-label="0">视频会议</el-checkbox>
 						</el-form-item>
 					</el-col>
-					
+
 					<el-col :xs="24" :sm="24" :md="12" :lg="8" :xl="8" class="mb10">
 						<el-form-item label="评选地点：" prop="Location">
 							<el-input v-model="ruleForm.Location"></el-input>
@@ -38,11 +38,16 @@
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10">
 						<el-form-item label="项目内容：" prop="Content">
-							<vue-ueditor-wrap :editor-id="`editor-content`"  
-								:editor-dependencies="['ueditor.config.js','ueditor.all.min.js','xiumi/xiumi-ue-dialog-v5.js','xiumi/xiumi-ue-v5.css']"
-								v-model="ruleForm.Content" 
-								:config="{UEDITOR_HOME_URL:'/ueditor/',serverUrl:`${baseUrl}/v1/common/editor/${getUserInfos.appid}`,headers:{'Authorization':token,Appid:getUserInfos.appid}}" 
-								></vue-ueditor-wrap>
+							<vue-ueditor-wrap
+								:editor-id="`editor-content`"
+								:editor-dependencies="['ueditor.config.js', 'ueditor.all.min.js', 'xiumi/xiumi-ue-dialog-v5.js', 'xiumi/xiumi-ue-v5.css']"
+								v-model="ruleForm.Content"
+								:config="{
+									UEDITOR_HOME_URL: '/ueditor/',
+									serverUrl: `${baseUrl}/v1/common/editor/${getUserInfos.appid}`,
+									headers: { Authorization: token, Appid: getUserInfos.appid },
+								}"
+							></vue-ueditor-wrap>
 						</el-form-item>
 					</el-col>
 					<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10">
@@ -57,9 +62,13 @@
 									:on-remove="onRemove"
 									:file-list="FilesList"
 									:accept:="`image/png, image/jpeg,image/bmp,image/jpg,application/pdf,application/docx,application/doc,application/xls,application/xlsx`"
-									multiple show-file-list>
+									multiple
+									show-file-list
+								>
 									<template #default>
-										<el-button><el-icon class="el-icon--right"><Upload /></el-icon>上传</el-button>
+										<el-button
+											><el-icon class="el-icon--right"><Upload /></el-icon>上传</el-button
+										>
 									</template>
 								</el-upload>
 							</div>
@@ -71,12 +80,12 @@
 								<Plus />
 							</el-icon>
 							添加标的物
-						</el-button >
+						</el-button>
 						<el-table :data="tableData.data" v-loading="tableData.loading" style="width: 100%" size="small" border stripe highlight-current-row>
 							<el-table-column type="index" label="序号" align="right" width="70" fixed />
 							<el-table-column prop="No" label="品目号" width="70"></el-table-column>
 							<el-table-column prop="Sn" label="物资编码" width="120" show-overflow-tooltip></el-table-column>
-							
+
 							<el-table-column prop="Name" label="名称" show-overflow-tooltip></el-table-column>
 							<el-table-column prop="Remark" label="备注" show-overflow-tooltip></el-table-column>
 							<el-table-column prop="Qty" label="数量" show-overflow-tooltip></el-table-column>
@@ -87,7 +96,8 @@
 										bg
 										type="primary"
 										@click="onModelEdit(false, scope.row, scope.$index)"
-										v-auth:[$parent.moduleKey]="'btn.ProjectLineEdit'">
+										v-auth:[$parent.moduleKey]="'btn.ProjectLineEdit'"
+									>
 										<el-icon>
 											<Edit />
 										</el-icon>
@@ -107,17 +117,32 @@
 				<el-row :gutter="20">
 					<el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="mb10"
 						><el-form-item label="报名开始时间：" prop="StartTime">
-							<el-date-picker v-model="ruleForm.StartTime" type="datetime" placeholder="报名开始时间" style="width: 100%"></el-date-picker> </el-form-item
+							<el-date-picker
+								v-model="ruleForm.StartTime"
+								type="datetime"
+								placeholder="报名开始时间"
+								style="width: 100%"
+							></el-date-picker> </el-form-item
 					></el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="mb10"
 						><el-form-item label="报名结束时间：" prop="EndTime">
-							<el-date-picker v-model="ruleForm.EndTime" type="datetime" placeholder="报名结束时间" style="width: 100%"></el-date-picker> </el-form-item
+							<el-date-picker
+								v-model="ruleForm.EndTime"
+								type="datetime"
+								placeholder="报名结束时间"
+								style="width: 100%"
+							></el-date-picker> </el-form-item
 					></el-col>
 				</el-row>
 				<el-row :gutter="20">
 					<el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="mb10"
 						><el-form-item label="招标开始时间：" prop="BeginTime">
-							<el-date-picker v-model="ruleForm.BeginTime" type="datetime" placeholder="招标开始时间" style="width: 100%"></el-date-picker> </el-form-item
+							<el-date-picker
+								v-model="ruleForm.BeginTime"
+								type="datetime"
+								placeholder="招标开始时间"
+								style="width: 100%"
+							></el-date-picker> </el-form-item
 					></el-col>
 					<el-col :xs="24" :sm="12" :md="12" :lg="6" :xl="6" class="mb10"
 						><el-form-item label="招标结束时间：" prop="FinishTime">
@@ -138,6 +163,133 @@
 							></el-date-picker> </el-form-item
 					></el-col>
 				</el-row>
+				<el-divider content-position="left">资格评审</el-divider>
+				<el-form size="small" label-width="90px" :inline="true">
+					<el-form-item>
+						<el-button size="small" type="primary" @click="onOpenItemDialog('zgps', true)">
+							<el-icon>
+								<CirclePlusFilled />
+							</el-icon>
+							{{ $t('message.action.add') }}
+						</el-button>
+						<el-button size="small" @click="onOpenListDialog('zgps')">
+							<el-icon><Edit /></el-icon>
+							获取
+						</el-button>
+					</el-form-item>
+					<el-form-item></el-form-item>
+				</el-form>
+				<el-table :data="zgTableData.data" v-loading="zgTableData.loading" style="width: 100%" size="small" border stripe highlight-current-row>
+					<el-table-column type="index" label="序号" align="right" width="70" fixed />
+					<el-table-column prop="Content" label="评审内容" show-overflow-tooltip />
+					<el-table-column prop="Standard" label="评审标准" show-overflow-tooltip />
+					<el-table-column :label="$t('message.action.operate')" :width="proxy.$calcWidth(160)" fixed="right">
+						<template #default="scope">
+							<el-button text bg type="primary" @click="onOpenItemDialog('zgps', false, scope.row)" v-auth:[$parent.moduleKey]="'btn.CategoryEdit'">
+								<el-icon>
+									<Edit />
+								</el-icon>
+								{{ $t('message.action.edit') }}
+							</el-button>
+							<el-button
+								text
+								bg
+								type="danger"
+								@click="onModelLineDel('zgps', scope.$index, scope.row.Id)"
+								v-auth:[$parent.moduleKey]="'btn.SettingLineDel'"
+							>
+								<el-icon>
+									<CloseBold />
+								</el-icon>
+								{{ $t('message.action.delete') }}
+							</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<el-divider content-position="left">技术评审</el-divider>
+				<el-form size="small" label-width="90px" :inline="true">
+					<el-form-item>
+						<el-button size="small" type="primary" @click="onOpenItemDialog('jsps', true)">
+							<el-icon>
+								<Plus />
+							</el-icon>
+							{{ $t('message.action.add') }}
+						</el-button>
+						<el-button size="small" @click="onOpenListDialog('jsps')">
+							<el-icon><Edit /></el-icon>
+							获取
+						</el-button>
+					</el-form-item>
+					<el-form-item></el-form-item>
+				</el-form>
+				<el-table :data="jsTableData.data" v-loading="jsTableData.loading" style="width: 100%" size="small" border stripe highlight-current-row>
+					<el-table-column type="index" label="序号" align="right" width="70" fixed />
+					<el-table-column prop="Content" label="评审内容" show-overflow-tooltip />
+					<el-table-column prop="Standard" label="评审标准" show-overflow-tooltip />
+					<el-table-column prop="TechnicalMaxScore" label="最高分数" show-overflow-tooltip />
+					<el-table-column :label="$t('message.action.operate')" :width="proxy.$calcWidth(160)" fixed="right">
+						<template #default="scope">
+							<el-button text bg type="primary" @click="onOpenItemDialog('jsps', false, scope.row)" v-auth:[$parent.moduleKey]="'btn.CategoryEdit'">
+								<el-icon>
+									<Edit />
+								</el-icon>
+								{{ $t('message.action.edit') }}
+							</el-button>
+							<el-button
+								text
+								bg
+								type="danger"
+								@click="onModelLineDel('jsps', scope.$index, scope.row.Id)"
+								v-auth:[$parent.moduleKey]="'btn.SettingLineDel'"
+							>
+								<el-icon>
+									<CloseBold />
+								</el-icon>
+								{{ $t('message.action.delete') }}
+							</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<el-divider content-position="left">经济评审</el-divider>
+				<el-form ref="jjFormRef" :model="jjForm" size="small" v-loading="loading">
+					<el-row :gutter="20">
+						<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10" :offset="1"
+							><el-form-item label="采购控制价：" prop="PurchasePrice"
+								><el-input-number v-model="jjForm.PurchasePrice" :min="0" controls-position="right" :precision="2" /> </el-form-item
+						></el-col>
+						<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10" :offset="1"
+							><el-form-item label="价格评审得分策略：">
+								<el-radio-group v-model="jjForm.ScoreMode">
+									<el-radio :label="0">价格排名打分</el-radio>
+									<el-radio :label="1">基础价格打分</el-radio>
+								</el-radio-group>
+							</el-form-item></el-col
+						>
+						<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10" :offset="1" v-if="jjForm.ScoreMode == 0">
+							<div class="mb10">评审价格从低至高排列（最低价为第一名），第一名供应商价格得分为满分；</div>
+							<div class="mb10">
+								从第二名起，价格得分减少<span><el-input-number v-model="jjForm.PriceScore" :min="0" :max="100" controls-position="right" /> </span
+								>分。超出采购控制价的供应商得零分。
+							</div>
+						</el-col>
+						<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10" :offset="1" v-if="jjForm.ScoreMode == 1">
+							<div class="mb10">
+								价格评审最高分的<span><el-input-number v-model="jjForm.PricePercentage" :min="0" :max="100" controls-position="right" /> </span>
+								%为基础价格得分;
+							</div>
+							<div class="mb10">
+								评审报价比招标控制价每下浮1个百分点，则得分增加<span
+									><el-input-number v-model="jjForm.QualificationScore" :min="0" :max="100" controls-position="right" />
+								</span>
+								分;
+							</div>
+						</el-col>
+						<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" :offset="1" class="mb10">
+							<div class="mb10">本项目技术评审最高分：{{ jjForm.TechnicalScore }}分，最终评审价格最高分：{{ jjForm.TechnicalMaxScore }} 分。</div>
+							<div class="mb10">如需修改请返回调整技术评审各得分项。</div>
+						</el-col>
+					</el-row>
+				</el-form>
 				<el-row :gutter="20">
 					<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb10 ml30">
 						<h3>注意：如果此公开比选项目不足三家供应商参与，是否自动转为竞争性谈判项目？</h3>
@@ -158,6 +310,8 @@
 		</el-dialog>
 	</div>
 	<lineEditDlg ref="lineEditDlgRef" />
+	<editItemDlg ref="editItemDlgRef" />
+	<editLineListDlg ref="editLineListDlgRef" />
 </template>
 
 <script lang="ts">
@@ -169,6 +323,8 @@ import { useI18n } from 'vue-i18n';
 import lineEditDlg from './projectLineEdit.vue';
 import { useStore } from '/@/store/index';
 import { Session } from '/@/utils/storage';
+import editLineListDlg from './projectLineList.vue';
+import editItemDlg from './templateEdit.vue';
 export default {
 	name: 'projectEdit',
 	setup() {
@@ -181,7 +337,8 @@ export default {
 		const getUserInfos = computed(() => {
 			return store.state.userInfos.userInfos;
 		});
-
+		const editItemDlgRef = ref();
+		const editLineListDlgRef = ref();
 		const state = reactive({
 			moduleKey,
 			isShowDialog: false,
@@ -209,6 +366,26 @@ export default {
 				ReviewTime: '', //评选时间
 				BidOpenTime: '',
 				ProjectLineList: [],
+				ProjectSettingLineList: [],
+			},
+			jjForm: {
+				Id: '0',
+				Kind: 'jjps',
+				PurchasePrice: 0, //采购控制价
+				ScoreMode: 0, //价格得分模式
+				PriceScore: 0, //价格得分减少
+				PricePercentage: 50, //价格百分比
+				QualificationScore: 0, //价格分数
+				TechnicalScore: 0,
+				TechnicalMaxScore: 0,
+			},
+			zgTableData: {
+				data: [],
+				loading: false,
+			},
+			jsTableData: {
+				data: [],
+				loading: false,
 			},
 			tableData: {
 				data: [],
@@ -264,40 +441,48 @@ export default {
 				},
 			],
 		});
-		
+
 		//上传成功
 		const onSuccessFile = (file: UploadFile) => {
-			debugger
 			state.Files.push(file.data.src);
 			const imgPath = { url: state.homeBaseUrl + file.data.src };
 			state.FilesList.push(imgPath);
 		};
 		//删除上传文件
 		const onRemove = (file: UploadFile) => {
-			console.log(file)
+			console.log(file);
 			let removeUrl = file.url.substring(file.url.indexOf('/static/upload/image/'), file.url.length);
 			for (let i = 0; i < state.FilesList.length; i++) {
 				if (state.FilesList[i] == removeUrl) {
 					state.FilesList.splice(i, 1);
-					break
+					break;
 				}
 			}
 		};
-
+		const getScore = () => {
+			if (state.jsTableData.data && state.jsTableData.data.length > 0) {
+				console.log(state.jjForm);
+				state.jjForm.TechnicalScore = 0;
+				for (let item of state.jsTableData.data) {
+					state.jjForm.TechnicalScore += item.TechnicalMaxScore;
+				}
+				state.jjForm.TechnicalMaxScore = 100 - state.jjForm.TechnicalScore;
+			}
+		};
 		//预览文件
-		const onPreview =(uploadFile:any) => {
+		const onPreview = (uploadFile: any) => {
 			// console.log(uploadFile);
 			// 当格式为图片就预览图片，否则下载文件
 			let filename = uploadFile.name;
 			let fileurl = uploadFile.url;
-			let fileExtension = "";
+			let fileExtension = '';
 
 			// 校检文件类型
-			var imageTypes = ["png", "jpg", "jpeg", "gif"];
-			if (filename.lastIndexOf(".") > -1) {
-				fileExtension = filename.slice(filename.lastIndexOf(".") + 1);
+			var imageTypes = ['png', 'jpg', 'jpeg', 'gif'];
+			if (filename.lastIndexOf('.') > -1) {
+				fileExtension = filename.slice(filename.lastIndexOf('.') + 1);
 			}
-			const isTypeOk = imageTypes.some(type => {
+			const isTypeOk = imageTypes.some((type) => {
 				if (fileExtension && fileExtension.indexOf(type) > -1) return true;
 				return false;
 			});
@@ -311,10 +496,10 @@ export default {
 				//下载文件
 				dialogVisible.value = false;
 				// openWindow(fileurl, { target: "_self" });
-				window.open(fileurl, "_self");
+				window.open(fileurl, '_self');
 			}
 		};
-		
+
 		//招标方式
 		const getBiddMethod = async () => {
 			try {
@@ -331,7 +516,7 @@ export default {
 			lineEditDlgRef.value.openDialog(item, isadd, index);
 		};
 		// 打开弹窗
-		const openDialog = (kind: string, id: string) => {
+		const openDialog = async (kind: string, id: string) => {
 			getBiddMethod();
 			if (id != '0') {
 				GetByIdRow(id);
@@ -349,7 +534,6 @@ export default {
 				if (res.errcode != 0) {
 					return;
 				}
-				state.ruleForm = res.data;
 				res.data.ProjectType = res.data.ProjectType.toString();
 				if (res.data.ProjectLineList) {
 					state.tableData.data = res.data.ProjectLineList;
@@ -363,6 +547,21 @@ export default {
 						state.FilesList.push(image);
 					}
 				}
+				if (state.ruleForm.ProjectSettingLineList) {
+					for (let item of state.ruleForm.ProjectSettingLineList) {
+						if (item.Kind == 'zgps') {
+							state.zgTableData.data.push(item);
+						} else if (item.Kind == 'jsps') {
+							state.jsTableData.data.push(item);
+						} else if (item.Kind == 'jjps') {
+							state.jjForm = item;
+						}
+					}
+				}
+				state.ruleForm = JSON.parse(JSON.stringify(res.data));
+
+				// 计算得分
+				getScore();
 			} finally {
 				state.isShowDialog = true;
 			}
@@ -374,6 +573,8 @@ export default {
 			state.FilesList = [];
 			state.Files = [];
 			state.tableData.data = [];
+			state.jsTableData.data = [];
+			state.zgTableData.data = [];
 			state.isShowDialog = false;
 		};
 		const onLoadTable = () => {
@@ -395,6 +596,9 @@ export default {
 						state.ruleForm.ProjectType = parseInt(state.ruleForm.ProjectType);
 						state.ruleForm.BidOpenTime = state.ruleForm.BeginTime;
 						state.ruleForm.ProjectLineList = state.tableData.data;
+						state.ruleForm.ProjectSettingLineList = [...state.zgTableData.data, ...state.jsTableData.data];
+						state.jjForm.Id = state.jjForm.Id.toString();
+						state.ruleForm.ProjectSettingLineList.push(state.jjForm);
 						const res = await proxy.$api.erp.project.save(state.ruleForm);
 						if (res.errcode == 0) {
 							onLoadTable();
@@ -432,6 +636,44 @@ export default {
 				return false;
 			});
 		};
+		const onModelLineDel = (kind: string, index: number, Id: number) => {
+			ElMessageBox.confirm(`确定要删除这条记录吗?`, '提示', {
+				confirmButtonText: '确认',
+				cancelButtonText: '取消',
+				type: 'warning',
+			}).then(async () => {
+				if (Id == 0) {
+					if (kind == 'jsps') {
+						state.jsTableData.data.splice(index, 1);
+					} else if (kind == 'zgps') {
+						state.zgTableData.data.splice(index, 1);
+					}
+				} else {
+					try {
+						const res = await proxy.$api.erp.projectsettingline.delete(Id);
+						if (res.errcode == 0) {
+							if (kind == 'jsps') {
+								state.jsTableData.data.splice(index, 1);
+							} else if (kind == 'zgps') {
+								state.zgTableData.data.splice(index, 1);
+							}
+						}
+					} finally {
+					}
+				}
+				return false;
+			});
+		};
+		const onOpenItemDialog = (kind: string, isAdd: boolean, item: object) => {
+			// let model = {};
+			// if (item) {
+			// 	model = JSON.parse(JSON.stringify(item));
+			// }
+			editItemDlgRef.value.openDialog(kind, isAdd, item, false);
+		};
+		const onOpenListDialog = (kind: string) => {
+			editLineListDlgRef.value.openDialog(kind, false);
+		};
 		// 页面加载时
 		onMounted(() => {});
 		return {
@@ -448,15 +690,23 @@ export default {
 			onModelDel,
 			onModelEdit,
 			rules,
+			editItemDlgRef,
+			editLineListDlgRef,
 			lineEditDlgRef,
 			getUserInfos,
 			onSubmit,
+			onOpenListDialog,
+			onOpenItemDialog,
+			onModelLineDel,
+			getScore,
 			...toRefs(state),
 		};
 	},
 	components: {
 		Upload,
 		lineEditDlg,
+		editItemDlg,
+		editLineListDlg,
 	},
 };
 </script>
