20bb587e8b1afd123071575032142fc0f7d0ea55
997735419 <*yellbuy997735419>
2024-02-06 09:05:27 +0800

    瑞海更新

--- src/views/mcs/common/vehicle/component/vehicleEdit.vue
+++ src/views/mcs/common/vehicle/component/vehicleEdit.vue
@@ -127,10 +127,17 @@
 						<el-form-item label="联系人" prop="Linkman"> <el-input v-model="ruleForm.Linkman" placeholder="联系人"></el-input> </el-form-item
 					></el-col>
 					<el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8" class="mb20">
-						<el-form-item label="电话" prop="Phone"> <el-input v-model="ruleForm.Phone" placeholder="电话"></el-input> </el-form-item></el-col>
+						<el-form-item label="电话" prop="Phone"> <el-input v-model="ruleForm.Phone" placeholder="电话"></el-input> </el-form-item>
+					</el-col>
 						<el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8" class="mb20">
-						<el-form-item label="司机电话" prop="DriverMobile" required> <el-input v-model="ruleForm.DriverMobile" placeholder="司机电话"></el-input> </el-form-item></el-col>	
-					</el-row>
+						<el-form-item label="司机电话" prop="DriverMobile" required> <el-input v-model="ruleForm.DriverMobile" placeholder="司机电话"></el-input>
+						</el-form-item>
+					</el-col>	
+					<el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8" class="mb20">
+						<el-form-item label="司机姓名" prop="Driver" required> <el-input v-model="ruleForm.Driver" placeholder="司机电话"></el-input> 
+						</el-form-item>
+					</el-col>	
+				</el-row>
 			</el-form>
 			<template #footer>
 				<span class="dialog-footer">
