1fc1f395fb493065961ac30c02b6d07d4def5560
YELLBUY <19892257@qq.com>
2024-03-29 15:12:24 +0800

    mcs完善

--- src/api/erp/waybill.ts
+++ src/api/erp/waybill.ts
@@ -49,7 +49,7 @@ export default {
     },
 
     /**
-     * 获取大屏运量和计划统计
+     * 获取大屏运量和计划统计数据
      * @param scopeMode：模式，0按scopeLevel，多少级有多少个Filter参数；1按scopeKind，始终3个参数，按所在层级不足时填充0
      * @param scopeValue：层级值，0：当前用户所在层级，1：系统，2：应用，3：租户，4：用户
      * @param params 要传的参数值
