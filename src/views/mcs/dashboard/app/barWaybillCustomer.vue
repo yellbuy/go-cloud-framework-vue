d009c93ca0f8a29d2dac8230022356ee9bcbc192
YELLBUY <19892257@qq.com>
2024-05-31 08:41:12 +0800

    mcs 完善

--- src/views/mcs/dashboard/app/barWaybillCustomer.vue
+++ src/views/mcs/dashboard/app/barWaybillCustomer.vue
@@ -31,7 +31,7 @@ export default {
             var str =
               parms[0].axisValue +
               '</br>' +
-              parms[0].value +
+              (parms[0].value-12).toFixed(2) +
               ' 万吨'
             return str
           },
