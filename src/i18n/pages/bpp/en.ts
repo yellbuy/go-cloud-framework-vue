3dbc1264fc4fb75e3961c4bee1d2b1397916de5e
YELLBUY <19892257@qq.com>
2024-08-09 16:48:31 +0800

    element plus更新至2.8.0

--- src/i18n/pages/bpp/en.ts
+++ src/i18n/pages/bpp/en.ts
@@ -3,6 +3,23 @@ export default {
 	router: {
 		registerTenant: 'register teant',
 		registerUser: 'register user',
+		home:'home',
+		projectManagement:'project management',
+		projectTender:'project tender',
+		parameterTemplate:'parameter template',
+		projectEnd:'project end',
+		supplierManagement:'supplier management',
+		supplierAudit:'supplier audit',
+		supplierType:'supplier type',
+		expertManagement:'expert management',
+		expertList:'expert list',
+		projectManager:'project manager',
+		message:'message',
+		tenderee:'tenderee',
+		tenderer:'tenderer',
+		expert:'expert',
+		agency:'agency',
+		supervisor:'supervisor',
 	},
 	field:{
 		
--- src/i18n/pages/bpp/zh-tw.ts
+++ src/i18n/pages/bpp/zh-tw.ts
@@ -3,6 +3,23 @@ export default {
 	router: {
 		registerTenant: '招投标公司注册',
 		registerUser: '专家注册',
+		home:'首页',
+		projectManagement:'项目管理',
+		projectTender:'招标项目',
+		parameterTemplate:'参数模板',
+		projectEnd:'已结束项目',
+		supplierManagement:'供应商管理',
+		supplierAudit:'基本信息变更申请',
+		supplierType:'供应商类型管理',
+		expertManagement:'专家管理',
+		expertList:'专家列表',
+		projectManager:'项目管理员',
+		message:'站内消息',
+		tenderee:'招标方',
+		tenderer:'投标方',
+		expert:'专家',
+		agency:'代理机构',
+		supervisor:'监审',
 	},
 	field: {
 
