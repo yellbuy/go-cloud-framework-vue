32323a541062a29ab80b921c83bfeb134a988727
YELLBUY <19892257@qq.com>
2025-03-26 16:58:14 +0800

    res完善

--- src/views/res/dashboard/street/index.vue
+++ src/views/res/dashboard/street/index.vue
@@ -26,14 +26,14 @@
       </div>
       <div>
         <el-row :gutter="0">
-          <el-col :span="7">
+          <el-col :span="6">
             <div class="">
               <Title title="乡情介绍"/>
               <div class="target-text">
-                <div class="m20">
-                  大龙潭彝族乡，隶属于四川省攀枝花市仁和区，地处仁和区东南部，东隔金沙江与会理市相望，南与平地镇相连，西与大田镇、仁和镇毗邻，北与金江镇接壤。
-                  辖区总面积243.69平方千米。截至2018年末，大龙潭彝族乡户籍人口15650人。原属云南省永仁县第三区。1984年，改为大龙潭彝族乡。
-                  截至到2020年6月，大龙潭彝族乡下辖6个行政村， 乡人民政府驻裕民街43号。
+                <div class="margin">
+                  大龙潭彝族乡为彝、汉族杂居的民族乡。最高海拔2105米，最低海拔970米，乡境内多为山地，立体气候明显，属南亚热带半干旱季风气候，年平均气温20.5℃，年降雨量800毫米，无霜期长达350天左右。辖区面积242.8平方公里，下辖裕民村、拉鲊村、新街村、干坝子村、大龙潭村、混撒拉村6个行政村，共55个村民小组，常住人口3712户14321人。
+                  <br/>总村数6个，总户数3712户14321人。
+
                 </div>
               </div>
             </div>
@@ -69,7 +69,7 @@
                     </div>
                   </el-col>
                   <el-col :span="12">
-                    <div style="text-align: center" class="margin-top-xl">
+                    <div style="text-align: center">
                       <radarEchart :style="'height:13rem'" :indicatorNameData="[{ name: '组织强', max: 100 },  { name: '治理强', max: 100 }]" :series1ValueData="[100,100]" :series2ValueData="[80,90]"></radarEchart>
                       <div class="margin-top-xs">两强</div>
                     </div>
@@ -78,83 +78,144 @@
               </div>
             </div>
           </el-col>
-          <el-col :span="10">
+          <el-col :span="12">
             <div class="margin-top">
               <div class="text-center">
-                <Label :text="'总户数'" :title="10000" class="margin-lr-10"/>
-                <Label :color="'#FCAE26FF'" :text="'达标村数'" :title="12" class="margin-lr-10"/>
-                <Label :color="'#1AFD9BFF'" :text="'占比'" :title="'20%'" class="margin-lr-10" @click="onClickPleaseWait('查看未达标户数')"/>
+                <Label :text="'总户数'" :title="3712" class="margin-lr-10"/>
+                <Label :color="'#FCAE26FF'" :text="'达标村数'" :title="1" class="margin-lr-10"/>
+                <Label :color="'#1AFD9BFF'" :text="'占比'" :title="'16.7%'" class="margin-lr-10" @click="onClickPleaseWait('查看未达标户数')"/>
               </div>
             </div>
-            <div class="column-center margin-bottom">
+            <div >
               <antvImageMap :areaCode="areaCode" areaGoTo="village"/>
             </div>
+            <div >
+              <el-row style="z-index: 2000;">
+              <el-col :span="12">
+                <div class="target-header margin-top" @click="onClickPleaseWait('查看帮扶家庭户')">
+                <div class="target-title">帮扶户数统计</div>
+              </div>
+              <div class="target-content target-container-h3" @click="onClickPleaseWait('查看帮扶家庭户')">
+                <dv-scroll-board :config="bfhsConfig" style="width:100%;height:22vh" @click="clickHandler"
+                                @mouseover="mouseoverHandler"/>
+              </div>
+              </el-col>
+              <el-col :span="12">
+                <div class="target-header margin-top" @click="onClickPleaseWait('查看“五好”不达标家庭户')">
+                  <div class="target-title" style="width:10vw">“五好”不达标户数</div>
+                </div>
+                <div class="target-content target-container-h3" @click="onClickPleaseWait('查看“五好”不达标家庭户')">
+                  <dv-scroll-board :config="whbdbConfig" style="width:100%;height:22vh" @click="clickHandler"
+                                  @mouseover="mouseoverHandler"/>
+                </div>
+              </el-col>
+            </el-row>
+            </div>
+            
+            
             
           </el-col>
-          <el-col :span="7">
+          <el-col :span="6">
             <div class="">
               <Title title="公共服务均等化"/>
               <div class="target-text">
-                <div class="-label">
-                  <el-row :gutter="0">
-                    <el-col :span="12">
-                      <div class="margin-right-10 margin-tb-10">
-                        <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'学前教育三年毛入园率(%)'"
-                                class="target-content "/>
-                      </div>
+                <div class="target-content padding">
+                  <el-row :gutter="8">
+                    <el-col :span="6">
+                        <div class=" padding-tb">
+                          <label>学前教育三年毛<br>入园率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />2%
+                        </div>
 
                     </el-col>
-                    <el-col :span="12">
-                      <div class="margin-right-10  margin-tb-10">
-                        <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'全民医疗保险参保率(%)'"
-                                class="target-content"/>
-                      </div>
+                    <el-col :span="6">
+                        <div class="padding-tb">
+                          <label>全民医疗保险<br>参保率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">98.5%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />3.5%
+                        </div>
                     </el-col>
-                  </el-row>
-                  <el-row :gutter="0">
-                    <el-col :span="12">
-                      <div class="margin-right-10">
-                        <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'特殊人群医疗保险参保率(%)'"
-                                class="target-content"/>
-                      </div>
+                    <el-col :span="6">
+                        <div class="padding-tb">
+                          <label>特殊人群医疗保险<br>参保率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />2%
+                        </div>
                     </el-col>
-                    <el-col :span="12">
-                      <div class="margin-right-10">
-                        <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'农村客运覆盖率(%)'" class="target-content"/>
-                      </div>
+                    <el-col :span="6">
+                        <div class="padding-tb">
+                          <label>农村客运<br>覆盖率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />
+                        </div>
                     </el-col>
                   </el-row>
                 </div>
-                <div class="margin-tb-10">
+                <!-- <div class="margin-tb-10">
                   实现了幼有善育、学有优教、病有良医、老有康养、住有宜居、弱有帮扶。 一所中小学和附属中心幼儿园。
                   实现了农村客运全覆盖,村村通邮。建有农业社会化服务站。
-                </div>
+                </div> -->
               </div>
             </div>
             <div class="">
               <Title title="基础设施一体化"/>
-              <el-row :gutter="0">
-                <el-col :span="12">
-                  <div class="target-text margin-right-10 margin-tb-10">
-                    <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'自来水普及率(%)'" class="target-content"/>
-                  </div>
-
-                </el-col>
-                <el-col :span="12">
-                  <div class="target-text margin-right-10  margin-tb-10">
-                    <Label1 :color="'#1AFD9BFF'" :text="'100'" :title="'自然村通硬化路率(%)'" class="target-content"/>
-                  </div>
-                </el-col>
-              </el-row>
-
-              <div class="target-text margin-tb-10">
-                文化健身等基础设施配套完善,公共基础设施管理、运行、养护机制完善健全。生活垃圾收转运处置体系行政村覆盖率100%。
+              <div class="target-text">
+                <div class="target-content padding">
+                  <el-row :gutter="0">
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>自来水普及率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />
+                      </div>
+                    </el-col>
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>自然村通硬化路率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">82.6%</label><i class="fa fa-arrow-down margin-left-sm margin-right-xs" style="font-size: 1rem; color: orange;" />17.4%
+                      </div>
+                    </el-col>
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>卫生厕所达标率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />2%
+                      </div>
+                    </el-col>
+                  </el-row>
+                </div>
               </div>
             </div>
             <div class="">
               <Title title="乡村治理现代化"/>
-              <div class="target-text margin-tb-10">
-                社会主义核心价值观深入人心，乡级综治中心规范化建设，建有数字乡村平台，实现雪亮工程全盖。矛盾纠纷妥善化解，自治、法治、德治相结合的治理体系基本构建，物质富足、精神富有、乡风文明。
+              <div class="target-text">
+                <div class="target-content padding">
+                  <el-row :gutter="0">
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>法律援助率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />2%
+                      </div>
+                    </el-col>
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>雪亮工程覆盖率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">100%</label><i class="fa fa-arrow-down margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />2%
+                      </div>
+                    </el-col>
+                    <el-col :span="8">
+                      <div class="padding-tb">
+                          <label>矛盾纠纷化解率</label>
+                          <br><br>
+                          <label style="color:#1AFD9BFF;font-size: 1.4rem">98%</label><i class="fa fa-arrow-up margin-left-sm margin-right-xs" style="font-size: 1rem; color: lightgreen;" />3%
+                      </div>
+                    </el-col>
+                  </el-row>
+                </div>
               </div>
             </div>
             <div class="">
@@ -163,6 +224,14 @@
                 产业发展规模化
               </div>
             </div>
+            <!-- <div class="target-header margin-top" @click="onClickPleaseWait">
+              <div class="target-title">帮扶户数统计</div>
+            </div>
+            <div class="target-content target-container-h3" @click="onClickPleaseWait">
+              <dv-scroll-board :config="bfhsConfig" style="width:100%;height:22vh" @click="clickHandler"
+                              @mouseover="mouseoverHandler"/>
+            </div> -->
+            
           </el-col>
         </el-row>
       </div>
@@ -194,16 +263,45 @@ export default {
       curTime: dayjs().format("YYYY年MM月DD日"),
       areaCode: route.query.areaCode,
       areaName: route.query.areaName,
-    })
+      //基础设施补短情况配置 改帮扶统计
+      bfhsConfig :{
+        header: ['村', '帮扶户数'],
+        data: [
+          ['混撒拉村', '<span style="color:#32c5e9;">80</span>'],
+          ['裕民村', '<span style="color:#9fe6b8;">160</span>'],
+          ['拉鲊村', '120'],
+          ['新街村', '<span style="color:#ff9f7f;">90</span>'],
+          ['干坝子村', '60'],
+          ['大龙潭村', '<span style="color:#e062ae;">75</span>'],
+        ],
+        index: true,
+        rowNum: 5,
+        columnWidth: [50],
+        align: ['center'],
+      },
+      //综合帮扶情况配置
+      whbdbConfig :{
+        header: ['村', '不达标户数'],
+        data: [
+          ['混撒拉村', '<span style="color:#32c5e9;">80</span>'],
+          ['裕民村', '<span style="color:#9fe6b8;">160</span>'],
+          ['拉鲊村', '120'],
+          ['新街村', '<span style="color:#ff9f7f;">90</span>'],
+          ['干坝子村', '60'],
+          ['大龙潭村', '<span style="color:#e062ae;">75</span>'],
+        ],
+    }
+  })
     //导航链接
     const onGoToLink = (url: string) => {
       router.push(url)
     }
     //指标解读
-    const onClickPleaseWait = (name:string) => {
+    const onClickPleaseWait = (name:string='') => {
       const html = `请等待，${name}功能正在开发中...`
       ElMessageBox.alert(html, '温馨提示', {dangerouslyUseHTMLString: true,type: 'info',})
     }
+    
     // 页面加载时
     onMounted(() => {
 
