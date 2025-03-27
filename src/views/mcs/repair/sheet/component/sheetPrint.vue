0c05412a986a6e91b8d0d0a642a303716a332760
YELLBUY <19892257@qq.com>
2025-03-27 09:17:23 +0800

    部分脚本cdn更新，mcs打印问题处理

--- src/views/mcs/repair/sheet/component/sheetPrint.vue
+++ src/views/mcs/repair/sheet/component/sheetPrint.vue
@@ -151,7 +151,7 @@ export default {
 			printJs({
 				printable: 'printRef',
 				type: 'html',
-				css: ['//at.alicdn.com/t/font_2298093_o73r8wjdhlg.css', 'https://unpkg.com/element-plus/es/theme-chalk/index.css'],
+				// css: ['//at.alicdn.com/t/font_2298093_o73r8wjdhlg.css', 'https://unpkg.com/element-plus/es/theme-chalk/index.css'],
 				scanStyles: false,
 				style: `@media print{.mb15{margin-bottom:15px;}.el-button--small i.iconfont{font-size: 12px !important;margin-right: 5px;}}`,
 			});
